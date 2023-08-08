a27d85ae2f7134a740fa6d5255985d5dd893b51c
leandrosfreitas <leandrosanfreitas@gmail.com>
2023-08-07 21:03:36 -0300

    conversor de centimetros para polegadas

---
+++ centimetro_em_polegada.py
@@ -0,0 +1,6 @@
+# transforma centimetros em polegadas
+numero = 0
+while numero <= 0:
+  numero = int(input("Digite um número em centímetro: "))
+  print("Digite um número positivo e maior que zero!")
+print(f"Número em polegada é: {numero/2.54}")
